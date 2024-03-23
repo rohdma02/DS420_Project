d6f4a8b3fec7bb9523b36a74cbaddee04e66e9d3
pierco3 <pierco02@luther.edu>
2024-03-23 17:25:34 -0500

    Rearranged contents of preparation and investigation files

--- creditcard_preparation.py
+++ creditcard_preparation.py
@@ -1,5 +1,7 @@
 # Mirror file for importing pipeline into other files
 
+from pathlib import Path
+
 import pandas as pd
 
 from sklearn.pipeline import Pipeline
@@ -9,9 +11,33 @@ from sklearn.impute import SimpleImputer
 
 
 
-def create_pipeline(features, categorical_features):
 
-    # Create a trasnformer pipeline
+# Step 2; based on housing_transformer_pipeline file
+
+def load_creditcard_data():
+
+    # filepath = Path("datasets/creditcard_2023.csv") 
+    # if not filepath.is_file():
+    #     Path("datasets").mkdir(parents=True, exist_ok=True)
+    #     url = "https://github.com/ageron/data/raw/main/housing.tgz" 
+    #     urllib.request.urlretrieve(url, filepath)
+    #     with tarfile.open(tarball_path) as housing_tarball:
+    #         housing_tarball.extractall(path="datasets") 
+
+    return pd.read_csv("creditcard_2023.csv").drop(['id'], axis=1)
+
+
+def create_pipeline():
+
+    features = ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',
+                'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19',
+                'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28',
+                'Amount']
+    
+    categorical_features = []
+
+
+    # Create a transformer pipeline
     features_transformer = Pipeline(steps=[('imputer', SimpleImputer(
         strategy='median')), ('scaler', StandardScaler())])
 
@@ -19,8 +45,9 @@ def create_pipeline(features, categorical_features):
     categorical_transformer = Pipeline(steps=[('imputer', SimpleImputer(
         strategy='most_frequent')), ('onehot', OneHotEncoder(handle_unknown='ignore'))])
 
-    preprocessor = ColumnTransformer(transformers=[(
-        'num', features_transformer, features), ('cat', categorical_transformer, categorical_features)])
+    preprocessor = ColumnTransformer(transformers=[
+        ('num', features_transformer, features),
+        ('cat', categorical_transformer, categorical_features)])
 
     # Create the final pipeline
     # add more steps later as we work on the model
@@ -29,22 +56,19 @@ def create_pipeline(features, categorical_features):
     return pipeline
 
 
-def main():
+def prepare_creditcard_data():
 
-    # Demo
+    # Main
 
-    csv_file = "creditcard_2023.csv"
+    creditcard_data = load_creditcard_data()
+    
 
-    #Read data from csv into DataFrame
-    df = pd.read_csv(csv_file)
+    X = creditcard_data.drop(['Class'], axis=1)
+    y = creditcard_data['Class']
 
-    X = df.drop(['id', 'Class'], axis=1)
-    y = df['Class']
+    
+    # pipeline = create_pipeline()
+    
+    # pipeline.fit(X,y)
 
-    features = ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',
-                'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19',
-                'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28',
-                'Amount']
-    categorical_features = []
-    pipeline = create_pipeline(features, categorical_features)
-    pipeline.fit(X,y)
+    return X, y
